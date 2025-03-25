ae733b34e54dd2a49d81d29a5236e01000098189
pathfinder <gmlt.rafael@gmail.com>
2025-03-25 12:03:12 -0300

    commit

--- labs/binary_counter/Basys_3/lab_top.sv
+++ labs/binary_counter/Basys_3/lab_top.sv
@@ -1,7 +1,7 @@
 module lab_top
 #(
     parameter CLK       = 100,
-              KEY       = 4,
+              KEY       = 5,
               SW        = 16,
               LED       = 16,
               DIGIT     = 4
@@ -25,3 +25,20 @@ output wire  [DIGIT-1:0] digit
 //assign abcdefgh         = '0;
 //assign led              = '0;
 //assign digit            = '0;
+
+    localparam CNT = $clog2 (CLK * 1000 *1000);
+    logic [CNT-1:0] cnt;
+
+    always_ff @(posedge clk or posedge rst) 
+    begin
+    if(rst)
+      cnt = '0;
+    else 
+      cnt = cnt + 1'b1;   
+    end
+
+    assign led = cnt[$left(cnt)-:4];
+    
+
+   
+endmodule
