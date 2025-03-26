0a5bcf1e451498a7e01d7d51fc1c2186f0f8f6d3
pathfinder <gmlt.rafael@gmail.com>
2025-03-26 20:43:58 -0300

    commit

--- labs/shift_reg/shift_reg.sv
+++ labs/shift_reg/shift_reg.sv
@@ -1,13 +1,31 @@
-module shift_reg 
+module lab_top
 #(
-    parameter LED = 8,
-              KEY = 4 
-) 
+    parameter CLK       = 100,
+              KEY       = 5,
+              SW        = 16,
+              LED       = 16,
+              DIGIT     = 4
+)
 (
-    input             rst, clk,
-    input        [KEY-1:0] key,
-    output logic [LED-1:0] led
+input                   clk,                       
+input                   rst,
+
+//- - - - - Keys,Switches,LEDs - - - - - - - 
+
+input        [KEY-1:0]   key,
+input        [SW- 1:0]   sw,
+output logic [LED-1:0]   led,
+
+//- - - - - Seven Seg Display - - - - - - - 
+
+output logic [      7:0] abcdefgh,
+output wire  [DIGIT-1:0] digit
 );
+
+//assign abcdefgh         = '0;
+//assign led              = '0;
+//assign digit            = '0;
+
 
 logic [31:0] cnt;
 
@@ -16,21 +34,24 @@ always_ff @(posedge clk or posedge rst)
       cnt <= '0;
     else
       cnt <= cnt + 1'b1;
-wire enable = (cnt[22:0] == '0);
+localparam W               = 8;     
+wire enable                = (cnt[25:0] == '0);
+wire button_on             = |key;
 
-wire button_on = |key;
-logic [LED-1:0] shift_reg;
+
+logic [W-1:0] shift_reg;
 
 always_ff @(posedge clk or posedge rst)
     if(rst)
-      //shift_reg <= '0 ;//normal
-      shift_reg <= 3;//for ring
-    else
-       shift_reg = {shift_reg[0], shift_reg [7:1] }; //ring
-       //shift_reg = {button_on,shift_reg[LED-1:1]}; //right
-       //shift_reg = {shift_reg[N-2:0], button_on}; //left
+      shift_reg <= '1 ;                             
+
+    else if (enable & ~sw[1] & sw[2])
+       shift_reg <= {shift_reg[W-2:0], button_on};  //left
+    else if (enable & sw[1] & sw[2])
+       shift_reg = {button_on,shift_reg[W-1:1]};    //right 
+    else if (enable & ~sw[2])
+       shift_reg = {shift_reg[0],shift_reg[W-1:1]}; //ring  
 
 assign led = shift_reg;
-
     
 endmodule
