582e9986d68e3b8e48e507a72cc9021c32b685c7
pathfinder <gmlt.rafael@gmail.com>
2025-05-21 16:46:08 -0300

    commit

--- UART/UART_filtr/README.md
+++ UART/UART_filtr/README.md
@@ -5,3 +5,9 @@
 For more info check
 
 [Determining Clock Accuracy Requirements for UART Communications](https://www.analog.com/en/resources/technical-articles/determining-clock-accuracy-requirements-for-uart-communications.html)
+
+# *Testbench*
+
+![TX-RX](UART_RX_TX.jpg)
+
+![Waveform](waveform.png)
--- UART/UART_filtr/testbench_RX_TX/uart_tx.v
+++ UART/UART_filtr/testbench_RX_TX/uart_tx.v
@@ -1,11 +1,16 @@
-module uart_tx(
-input        rst_i,
-input        clk_i,
-input        tick_i,
-input        data_valid_i, // new data is ready at the input
-input [7 :0] data_i,       // data input
-output       ready_rcv_o,  // ready to receive new data when all the old ones previously received have been sent to the receiver
-output       txd_o         // one bit to the output
+module uart_tx
+(
+input              rst_i,
+input              clk_i,
+input              tick_i,
+input              data_valid_i, 
+input      [7 :0]  data_i,       
+output             ready_rcv_o, 
+output             txd_o,
+output reg [9 :0]  data_buffer_tx,
+output reg [3 :0]  ticks_count_tx,
+output reg [3 :0]  data_count_tx, 
+output             fsm_state_tx        
 );
     
 localparam [0 :0] ST_IDLE = 1'b0;
@@ -17,16 +22,20 @@ reg [9 :0] tx_buf;        // vector with all bits: start, data, and stop
 reg [3 :0] tx_buf_cnt;    // counter of transmitted bits (from 0 to 9)
 reg [3 :0] tick_cnt;      // tick counter (from 0 to 15)
 reg        state; 
-        
-assign ready_rcv_o = !(data_valid_i | transfer_data); // the signal is 1 only if there is no data to transmit and valid data is available from the upstream interface
-assign txd_o       = tx_buf[0];                       // one bit to the output, right side shift register
+  
+assign fsm_state_tx = state;      
+assign ready_rcv_o  = !(data_valid_i | transfer_data); // the signal is 1 only if there is no data to transmit and valid data is available from the upstream interface
+assign txd_o        = tx_buf[0];                       // one bit to the output, right side shift register
     
 always@(posedge clk_i)
 begin
 if (rst_i)
   begin
     transfer_data <= 1'b0;
-    state         <= ST_IDLE;
+    state            <= ST_IDLE;
+    data_buffer_tx   <= 0;
+    ticks_count_tx   <= 0;
+    data_count_tx    <= 0;
   end
 else
   begin
@@ -35,13 +44,9 @@ else
     begin
         tick_cnt   <= 4'd0;
         tx_buf_cnt <= 4'd0;
-        tx_buf     <= (tick_i & transfer_data) ? {1'b1, tx_data, 1'b0}: {10{1'b1}}; // if there is data to transmit and tick is on, write to bit array
-                                                                                    // here it's the opposite: first stop bit, then data bits and start bit
-                                                                                    // because at the output the first is bit [0],
-                                                                                    // otherwise fill the whole vector with 11_1111_1111 because if zero,
-                                                                                    // the receiver can mistakenly take 0 as a start bit and start collecting incorrect data
+        tx_buf     <= (tick_i & transfer_data) ? {1'b1, tx_data, 1'b0}: {10{1'b1}}; 
         state      <= (tick_i & transfer_data) ? ST_TX : ST_IDLE;                   
-        if (data_valid_i)                                                           // signal — data at the input
+        if (data_valid_i)                                                           
             begin
               tx_data       <= data_i;                                              // write 8 data bits into data bit 
               transfer_data <= 1'b1;                                                // flag that data is ready to transmit, go to the next state
@@ -57,7 +62,8 @@ else
 			  if (tick_cnt == 4'd15)
                 begin
                   tx_buf_cnt <= tx_buf_cnt + 4'd1;                                  // after 15 ticks, means first of 10 bits is transmitted
-                  tx_buf     <= {1'b1, tx_buf[9 :1]};                               // bit [0] goes to output, right side  shift register
+                  tx_buf     <= {1'b1, tx_buf[9 :1]};
+                                                                                    // bit [0] goes to output, right side  shift register
                 end
             end
         if (tx_buf_cnt == 4'd10)                                                    // bit transmission counter: if equal to 10, all bits are transmitted
@@ -69,5 +75,11 @@ else
   endcase
   end
 end
-    
+always@(posedge clk_i)
+begin
+  data_buffer_tx <= tx_buf; 
+  ticks_count_tx <= tick_cnt;
+  data_count_tx  <= tx_buf_cnt;
+end 
+     
 endmodule 
